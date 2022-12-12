b699db3c1d736ab2be315e0178ceaf79491f56e4
mathieu <mathieu.cans@orange.com>
2022-12-12 13:35:32 +0100

    diplay zelda text

--- app/src/main/java/com/example/zeldacountdown/FirstFragment.kt
+++ app/src/main/java/com/example/zeldacountdown/FirstFragment.kt
@@ -35,6 +35,8 @@ class FirstFragment : Fragment() {
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+
+        binding.textviewFirst.text = "Sortie de zelda dans XXX jours et XX:XX:XX"
     }
 
     override fun onDestroyView() {
