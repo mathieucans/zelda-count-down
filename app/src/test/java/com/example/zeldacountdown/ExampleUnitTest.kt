b0a053d22f617e248477985ea101e737162761f6
mathieu <mathieu.cans@orange.com>
2022-12-12 16:42:42 +0100

    Display day left and increase text

--- app/src/test/java/com/example/zeldacountdown/ExampleUnitTest.kt
+++ app/src/test/java/com/example/zeldacountdown/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package com.example.zeldacountdown
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.time.LocalDate
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +12,24 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun zero_day_left_the_day_of_release() {
+        val today = LocalDate.parse("2023-05-12")
+        val daysLeft = ZeldaCountDown(today).daysLeft()
+        assertEquals(0, daysLeft)
     }
+
+    @Test
+    fun one_day_left_the_day_before_of_release() {
+        val today = LocalDate.parse("2023-05-11")
+        val daysLeft = ZeldaCountDown(today).daysLeft()
+        assertEquals(1, daysLeft)
+    }
+
+    @Test
+    fun `151 days left from 2022 - 12 - 12`() {
+        val today = LocalDate.parse("2022-12-12")
+        val daysLeft = ZeldaCountDown(today).daysLeft()
+        assertEquals(151, daysLeft)
+    }
+
 }
